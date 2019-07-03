33646a4676d65dd4064250095f842eca0f74a795
Timofey Shunin <galecore@yandex-team.ru>
2019-07-03 23:07:24 +0300

    Add logging

--- docsender.py
+++ docsender.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import csv
 import smtplib
 import os
@@ -6,14 +7,34 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
-from contextlib import redirect_stdout, redirect_stderr
 import argparse
 from datetime import datetime
 
 
-'''stupid python can not stand infinitives'''
-months = \
-{
+import logging
+
+logger = logging.getLogger('base')
+logger.setLevel(logging.DEBUG)
+
+# create file handler which logs even debug messages
+fh = logging.FileHandler('logfile.log')
+fh.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+#stupid python can not stand infinitives
+months = {
     1:"январь",
     2:"февраль",
     3:"март",
@@ -37,10 +58,11 @@ def send_mail(smtp_server, send_from, send_to, subject="", text="", files=None):
     msg['Subject'] = subject
     msg.attach(MIMEText(text))
 
-    for f in files or []:
+    for f in (files or []):
+        file_basename = basename(f)
         with open(f, "rb") as binary_file:
-            part = MIMEApplication(binary_file.read(), Name=basename(f))
-        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+            part = MIMEApplication(binary_file.read(), Name=file_basename)
+        part['Content-Disposition'] = 'attachment; filename="{}"'.format(file_basename)
         msg.attach(part)
 
     smtp_server.sendmail(send_from, send_to, msg.as_string())
@@ -68,65 +90,65 @@ def generate_subject_and_body(raw_code, file_code, subject_raw, body_raw):
 def process_csv(smtp_server, csvfile, config):
     '''Processes strings from csvfile and returns filename of a modified copy of csvfile'''
     with open("tmp_" + basename(csvfile.name), 'w', encoding="cp1251") as tmp:
-        reader = csv.reader(csvfile, delimiter=config["delimiter"])
-        writer = csv.writer(tmp, delimiter=config["delimiter"], lineterminator="\n")
+        reader = csv.reader(csvfile, delimiter=config.get("delimiter", ";"))
+        writer = csv.writer(tmp, delimiter=config.get("delimiter", ";"), lineterminator="\n")
         for i, row in enumerate(reader):
-            if (not row):
+            if not row:
                 continue
             raw_code, mode, timepoint, price, raw_recepients, company_name, file1, file2, file_code = row
             recepients = list(map(lambda string: string.strip(), raw_recepients.replace("*", "").split(",")))
-            send_from = config["from"]    
-            subject, body = generate_subject_and_body(raw_code, file_code, config["subject_raw"], config["body_raw"])
+            send_from = config.get("from", "")    
+            subject, body = generate_subject_and_body(raw_code, file_code, config.get("subject_raw", ""), config.get("body_raw", ""))
             files = [file1.strip(), file2.strip()]
 
             should_send = True
             if mode.strip() == 'NO_SENT':
                 timepoint = datetime.today().strftime("%d.%m.%Y %H:%M")
-                print("Sent email on line {}".format(i))
+                logger.debug("Sending email on line %s", i)
             elif mode.strip() == "TEST":
-                recepients = [config["test_recepient"]]
+                recepients = [config.get("test_recepient", "")]
                 timepoint = datetime.today().strftime("%d.%m.%Y %H:%M")
-                print("Sent email on line {}".format(i))                
+                logger.debug("Sending email on line %s", i)                
             else:
                 should_send = False
-                print("Ignoring line {}, mode = {}".format(i, mode))
+                logger.info("Ignoring line %s, mode = %s", i, mode)
+                writer.writerow([raw_code, mode, timepoint, price, raw_recepients, company_name, file1, file2, file_code])
+                continue 
 
-            
             try:
                 if should_send:
                     send_mail(smtp_server,
                         send_from=send_from, send_to=recepients, 
                         subject=subject, text=body, files=files)
-            except:
-                result = "ERROR"
-            else:
-                result = "SENT"
-            finally:
-                writer.writerow([raw_code, result, timepoint, price, raw_recepients, company_name, file1, file2, file_code])
+            except Exception as e:
+                logger.error("Error on line %s", i)
+                logger.exception(e)
+            
+                writer.writerow([raw_code, "ERROR", timepoint, price, raw_recepients, company_name, file1, file2, file_code])
+                continue
+            
+            logger.info("Sent message on line %s", i)
+            writer.writerow([raw_code, "SENT", timepoint, price, raw_recepients, company_name, file1, file2, file_code])
 
     return "tmp_" + basename(csvfile.name)
 
-def open_smtp_server(config):
-    '''Opens stmp server connection and logs in using credentials'''
-    smtp_server = smtplib.SMTP_SSL(config["host"], config["port"])
-    smtp_server.login(config["login"], config["password"])
-    return smtp_server
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("config", type=argparse.FileType('r', encoding="cp1251"), 
+    parser.add_argument("--config", type=argparse.FileType('r', encoding="cp1251"), 
         help="Config file path.")
-    parser.add_argument("csv", type=argparse.FileType('r', encoding="cp1251"), 
+    parser.add_argument("--csv", type=argparse.FileType('r', encoding="cp1251"), 
         help="Input CSV file path")
-
     args = parser.parse_args()
 
-    with open("logfile.log", "w", 1) as logger, redirect_stdout(logger), redirect_stderr(logger):
+    try:
         config = read_configuration(args.config)
-        smtp_server = open_smtp_server(config)
-        tmp_filename = process_csv(smtp_server, args.csv, config)
-        csv_filename = args.csv.name
-        args.csv.close()
-        os.replace(src=tmp_filename, dst=csv_filename)    
+        with smtplib.SMTP_SSL(config.get("host", ""), config.get("port", 0)) as smtp_server: 
+            smtp_server.login(config.get("login", ""), config.get("password", ""))
+            tmp_filename = process_csv(smtp_server, args.csv, config)
+            csv_filename = args.csv.name
+            args.csv.close()
+            os.replace(src=tmp_filename, dst=csv_filename)
+    except Exception as e:
+        logger.exception(e)
 
 main()
